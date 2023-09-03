a1d26e0d49f7780a6f9a8f1ab7f16de8639c5302
Djyauhen <110858895+Djyauhen@users.noreply.github.com>
2023-09-04 01:17:35 +0300

    Add files via upload

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,11 @@
 'use strict'
 const languageBtn = document.getElementById('language-button');
+const languageBtnSmall = document.getElementById('language-button-small');
+const nav = document.getElementById('nav');
+const smallMenu = document.getElementById('small-menu');
+const menuOpen = document.getElementById('menu-burger');
+const menuClose = document.getElementById('small-menu-close');
+const header = document.getElementById('header');
 let language = document.getElementById('language');
 
 function changeLanguage(lang) {
@@ -12,4 +18,16 @@ function changeLanguage(lang) {
 
 languageBtn.onclick = () => {
     changeLanguage(language.innerText);
+}
+
+languageBtnSmall.onclick = () => {
+    changeLanguage(language.innerText);
+}
+
+menuOpen.onclick = () => {
+        smallMenu.style.display = 'block';
+}
+
+menuClose.onclick = () => {
+        smallMenu.style.display = 'none';
 }
